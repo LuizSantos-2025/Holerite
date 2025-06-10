bf759000b802c364e7a1c6f754ab22ff2e5ef367
luiz Santos <santos.luizviana@gmail.com>
2025-06-09 21:01:06 -0300

    desenvolvendo, melhorando e criando funcionalidades no codigo

--- js/newHolerite.js
+++ js/newHolerite.js
@@ -1,22 +1,39 @@
 
+// Mapeando  o icone da lua do bootstrap Icon
 var day = document.querySelector("#dark");
 
+// criando um evento de click no icone da lua bootstap icon
 day.addEventListener("click", function() {
-document.body.classList.toggle("darkMode");
-console.log("Night Ativado");
 
+// aplicando a classe darkMode usando o metodo toggle chamando o body atraves da propriedade document
+document.body.classList.toggle("darkMode");
+
+// dentra ainda do evento de click que chama o darkMode. Adicionado a classe hide no icone da lua atraves da variavel day quey mapea o icone da lua
 day.classList.add("hide");
+
+// validaçao condicional que diz: se existe a classe hide faça... 
 if(("hide")){
-    night.classList.remove("hide");
+
+// ...Remova a classe hide do icone sol bootstrap icon
+night.classList.remove("hide");
 }
 })
+// mapeando o icone do sol 
 var night = document.querySelector("#light");
+
+// criando um evento de click no icone do sol
 night.addEventListener("click", function(){
+
+// aplicando a classe darkMode usando o metodo toggle chamando o body atraves da propriedade document
 document.body.classList.toggle("darkMode");
 
-console.log("Light Ativado");
+// aplicando a classe hide no icone so sol
 night.classList.add("hide");
+
+// condicional se existe a classe hide...
 if(("hide")){
+
+    // ...Remova a classe hide do icone da lua
     day.classList.remove("hide");
 }
 })
@@ -72,7 +89,7 @@ function salarioMinimo(salario){
 
 var arr = [0.075, 0.09, 0.12, 0.14];
 var liquido = salario - (salario*arr[0]);
-console.log(liquido);
+
 
 var body = document.querySelector("body");
 
@@ -90,7 +107,7 @@ criaP4.setAttribute("class","valorLiquido");
 var criaNo = document.createTextNode("Salario: R$ "+ salario);
 var criaNo2 = document.createTextNode("Desconto Inss: "+ (salario*arr[0]).toFixed(2));
 var criaNo3 = document.createTextNode("Aliquota R$ 7,5%");
-var criaNo4 = document.createTextNode("Liquido R$ " +liquido);
+var criaNo4 = document.createTextNode("Liquido R$ " +liquido.toFixed(2));
 
 criaDiv.appendChild(criaP);
 criaDiv.appendChild(criaP2);
@@ -108,6 +125,7 @@ body.appendChild(containerVazio);
 function salarioMedio(salario){
     
 var arr = [0.075, 0.09, 0.12, 0.14];
+var liquido = salario - (salario*arr[1]);
 
 var body = document.querySelector("body");
 
@@ -119,23 +137,29 @@ criaDiv.setAttribute("class","firstAliquota");
 var criaP  = document.createElement("p");
 var criaP2 = document.createElement("p");
 var criaP3 = document.createElement("p");
+var criaP4 = document.createElement("p");
+criaP4.setAttribute("class","valorLiquido");
 
 var criaNo = document.createTextNode("Salario: R$ "+ salario);
 var criaNo2 = document.createTextNode("Desconto Inss: "+ (salario*arr[1]).toFixed(2));
 var criaNo3 = document.createTextNode("Aliquota R$ 9%");
+var criaNo4 = document.createTextNode("Liquido R$ " +liquido.toFixed(2));
 
 criaDiv.appendChild(criaP);
 criaDiv.appendChild(criaP2);
 criaDiv.appendChild(criaP3);
+criaDiv.appendChild(criaP4);
 criaP.appendChild(criaNo);
 criaP2.appendChild(criaNo2);
 criaP3.appendChild(criaNo3);
+criaP4.appendChild(criaNo4);
 containerVazio.appendChild(criaDiv);
 body.appendChild(containerVazio);
 }
 
 function salarioAlto(salario){
 var arr = [0.075, 0.09, 0.12, 0.14];
+var liquido = salario - (salario*arr[2]);
 
 var body = document.querySelector("body");
 
@@ -147,23 +171,29 @@ criaDiv.setAttribute("class","firstAliquota");
 var criaP  = document.createElement("p");
 var criaP2 = document.createElement("p");
 var criaP3 = document.createElement("p");
+var criaP4 = document.createElement("p");
+criaP4.setAttribute("class","valorLiquido");
 
 var criaNo = document.createTextNode("Salario: R$ "+ salario);
 var criaNo2 = document.createTextNode("Desconto Inss: "+ (salario*arr[2]).toFixed(2));
 var criaNo3 = document.createTextNode("Aliquota R$ 12%");
+var criaNo4 = document.createTextNode("Liquido R$ " +liquido.toFixed(2));
 
 criaDiv.appendChild(criaP);
 criaDiv.appendChild(criaP2);
 criaDiv.appendChild(criaP3);
+criaDiv.appendChild(criaP4);
 criaP.appendChild(criaNo);
 criaP2.appendChild(criaNo2);
 criaP3.appendChild(criaNo3);
+criaP4.appendChild(criaNo4)
 containerVazio.appendChild(criaDiv);
 body.appendChild(containerVazio);
 }
 
 function tetoMax(salario) {
 var arr = [0.075, 0.09, 0.12, 0.14];
+var liquido = salario - (salario*arr[3]);
 
 var body = document.querySelector("body");
 
@@ -175,17 +205,22 @@ criaDiv.setAttribute("class","firstAliquota");
 var criaP  = document.createElement("p");
 var criaP2 = document.createElement("p");
 var criaP3 = document.createElement("p");
+var criaP4 = document.createElement("p");
+criaP4.setAttribute("class","valorLiquido");
 
 var criaNo = document.createTextNode("Salario: R$ "+ salario);
 var criaNo2 = document.createTextNode("Desconto Inss: "+ (salario*arr[3]).toFixed(2));
 var criaNo3 = document.createTextNode("Aliquota R$ 14%");
+var criaNo4 = document.createTextNode("Liquido R$ " +liquido.toFixed(2));
 
 criaDiv.appendChild(criaP);
 criaDiv.appendChild(criaP2);
 criaDiv.appendChild(criaP3);
+criaDiv.appendChild(criaP4);
 criaP.appendChild(criaNo);
 criaP2.appendChild(criaNo2);
 criaP3.appendChild(criaNo3);
+criaP4.appendChild(criaNo4);
 containerVazio.appendChild(criaDiv);
 body.appendChild(containerVazio);
 }
